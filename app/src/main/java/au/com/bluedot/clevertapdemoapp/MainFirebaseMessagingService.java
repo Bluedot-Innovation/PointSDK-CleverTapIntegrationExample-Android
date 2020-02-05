79d1823088a2416ec87dfa5a657272c1f059e117
sreeram82@gmail.com <ram@bluedot.io>
2020-02-05 16:56:04 +1100

    fix(MainFirebaseMessagingService): Removed unused code

--- app/src/main/java/au/com/bluedot/clevertapdemoapp/MainFirebaseMessagingService.java
+++ app/src/main/java/au/com/bluedot/clevertapdemoapp/MainFirebaseMessagingService.java
@@ -41,19 +41,4 @@ public class MainFirebaseMessagingService extends FirebaseMessagingService {
         super.onSendError(s, e);
         System.out.println("-- onSendError: " + s + " e: " + e.getMessage());
     }
-
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
-     */
-    @Override
-    public void onNewToken(String token) {
-     //   Log.d(TAG, "Refreshed token: " + token);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-       // sendRegistrationToServer(token);
-    }
 }
