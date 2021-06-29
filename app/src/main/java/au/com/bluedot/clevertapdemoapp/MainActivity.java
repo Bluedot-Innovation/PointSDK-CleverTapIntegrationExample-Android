f45eea4a6ab8d9c20fc4f06bb8f7d4eccc4434eb
Duncan Lau <duncan@bluedot.io>
2021-06-29 09:56:31 +0800

    Update Clevertap minimal app to utilize 15.3.4 of Android Point SDK

--- app/src/main/java/au/com/bluedot/clevertapdemoapp/MainActivity.java
+++ app/src/main/java/au/com/bluedot/clevertapdemoapp/MainActivity.java
@@ -1,17 +1,105 @@
 package au.com.bluedot.clevertapdemoapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.clevertap.android.sdk.CleverTapAPI;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.Nullable;
+
+import au.com.bluedot.point.net.engine.BDError;
+import au.com.bluedot.point.net.engine.GeoTriggeringService;
+import au.com.bluedot.point.net.engine.GeoTriggeringStatusListener;
+
+import static androidx.core.app.NotificationCompat.PRIORITY_MAX;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GeoTriggeringStatusListener {
+
+    Button bStartStopSDK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bStartStopSDK = findViewById(R.id.bStartStopSDK);
+        bStartStopSDK.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (GeoTriggeringService.isRunning()) {
+            bStartStopSDK.setText(R.string.stop_sdk);
+        } else {
+            bStartStopSDK.setText(R.string.start_sdk);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (GeoTriggeringService.isRunning()) {
+            GeoTriggeringService.stop(getApplicationContext(), this);
+            bStartStopSDK.setText(R.string.start_sdk);
+        } else {
+            Notification notification = createNotification();
+            GeoTriggeringService.builder()
+                    .notification(notification)
+                    .start(getApplicationContext(), this);
+            bStartStopSDK.setText(R.string.stop_sdk);
+        }
+    }
+
+    /**
+     * Creates notification channel and notification, required for foreground service notification.
+     * @return notification
+     */
+    private Notification createNotification() {
+        String channelId;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelId = "Bluedot" + getString(R.string.app_name);
+            String channelName = "Bluedot Service" + getString(R.string.app_name);
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.enableLights(false);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(false);
+            NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            Notification.Builder notification = new Notification.Builder(getApplicationContext(), channelId)
+                    .setContentTitle(getString(R.string.foreground_notification_title))
+                    .setContentText(getString(R.string.foreground_notification_text))
+                    .setStyle(new Notification.BigTextStyle().bigText(getString(R.string.foreground_notification_text)))
+                    .setOngoing(true)
+                    .setCategory(Notification.CATEGORY_SERVICE)
+                    .setSmallIcon(R.mipmap.ic_launcher);
+
+            return notification.build();
+        } else {
+
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext())
+                    .setContentTitle(getString(R.string.foreground_notification_title))
+                    .setContentText(getString(R.string.foreground_notification_text))
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.foreground_notification_text)))
+                    .setOngoing(true)
+                    .setCategory(Notification.CATEGORY_SERVICE)
+                    .setPriority(PRIORITY_MAX)
+                    .setSmallIcon(R.mipmap.ic_launcher);
+
+            return notification.build();
+        }
     }
 
     @Override
@@ -22,5 +110,14 @@ public class MainActivity extends AppCompatActivity {
             t.printStackTrace();
         }
         super.onNewIntent(intent);
+    }
+
+    @Override
+    public void onGeoTriggeringResult(@Nullable BDError bdError) {
+        if (bdError == null) return;
+
+        Toast.makeText(getApplicationContext(),
+                "GeoTrigger Start Error " + bdError.getReason(),
+                Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/au/com/bluedot/clevertapdemoapp/MainApplication.java
+++ app/src/main/java/au/com/bluedot/clevertapdemoapp/MainApplication.java
@@ -10,11 +10,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
+import android.widget.Toast;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
 import com.clevertap.android.sdk.ActivityLifecycleCallback;
 import com.clevertap.android.sdk.CleverTapAPI;
+
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,6 +30,7 @@ import au.com.bluedot.point.ServiceStatusListener;
 import au.com.bluedot.point.net.engine.BDError;
 import au.com.bluedot.point.net.engine.BeaconInfo;
 import au.com.bluedot.point.net.engine.FenceInfo;
+import au.com.bluedot.point.net.engine.InitializationResultListener;
 import au.com.bluedot.point.net.engine.LocationInfo;
 import au.com.bluedot.point.net.engine.ServiceManager;
 import au.com.bluedot.point.net.engine.ZoneInfo;
@@ -35,15 +40,10 @@ import static android.app.Notification.PRIORITY_MAX;
 /**
  * Created by Adil Bhatti on 17/05/16.
  */
-public class MainApplication extends Application implements ServiceStatusListener, ApplicationNotificationListener {
+public class MainApplication extends Application implements InitializationResultListener {
 
     private ServiceManager serviceManager;
-    private final String API_KEY = "Your API key"; //API key for the Point Demo App
-    private final boolean RESTART_MODE = false;
-    private NotificationChannel notificationChannel = null;
-    private final String EVENT_PLACE_ENTERED = "bluedot_place_entered";
-    private final String EVENT_PLACE_EXITED = "bluedot_place_exited";
-
+    private final String projectId = ""; //Project Id for the Point Demo App
 
     @Override
     public void onCreate() {
@@ -65,10 +65,7 @@ public class MainApplication extends Application implements ServiceStatusListene
             serviceManager = ServiceManager.getInstance(this);
 
             if(!serviceManager.isBlueDotPointServiceRunning()) {
-                // Setting Notification for foreground service, required for Android Oreo and above.
-                // Setting targetAllAPIs to TRUE will display foreground notification for Android versions lower than Oreo
-                serviceManager.setForegroundServiceNotification(createNotification(), false);
-                serviceManager.sendAuthenticationRequest(API_KEY,this, RESTART_MODE);
+                serviceManager.initialize(projectId, this);
             }
         }
         else
@@ -77,114 +74,8 @@ public class MainApplication extends Application implements ServiceStatusListene
         }
     }
 
-
     private void initCleverTap() {
         ActivityLifecycleCallback.register(this);
-    }
-
-    @Override
-    public void onCheckIntoFence(FenceInfo fenceInfo, ZoneInfo zoneInfo, LocationInfo locationInfo, Map<String, String> customDataMap, boolean b) {
-        sendCustomEvent(EVENT_PLACE_ENTERED, zoneInfo, customDataMap);
-    }
-
-    private void sendCustomEvent(String eventName, ZoneInfo zoneInfo, Map<String, String> customDataMap) {
-        sendCustomEvent(eventName, zoneInfo, -1, customDataMap);
-    }
-
-
-    private void sendCustomEvent(String eventName, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-        CleverTapAPI cleverTap = CleverTapAPI.getDefaultInstance(getApplicationContext());
-        HashMap<String, Object> checkInAction = new HashMap<String, Object>();
-        checkInAction.put("bluedot_zone_id", zoneInfo.getZoneId());
-        checkInAction.put("bluedot_zone_name", zoneInfo.getZoneName());
-        if(customDataMap != null && !customDataMap.isEmpty()) {
-            for(Map.Entry<String, String> data : customDataMap.entrySet()) {
-                checkInAction.put(data.getKey(), data.getValue());
-            }
-        }
-
-        if(dwellTime != -1) {
-            checkInAction.put("dwell_time", dwellTime);
-        }
-
-        cleverTap.pushEvent("bluedot_entry", checkInAction);
-    }
-
-    @Override
-    public void onCheckedOutFromFence(FenceInfo fenceInfo, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-        sendCustomEvent(EVENT_PLACE_EXITED, zoneInfo, dwellTime, customDataMap);
-    }
-
-    @Override
-    public void onCheckIntoBeacon(BeaconInfo beaconInfo, ZoneInfo zoneInfo, LocationInfo locationInfo, Proximity proximity, Map<String, String> customDataMap, boolean b) {
-        sendCustomEvent(EVENT_PLACE_ENTERED, zoneInfo, customDataMap);
-    }
-
-    @Override
-    public void onCheckedOutFromBeacon(BeaconInfo beaconInfo, ZoneInfo zoneInfo, int dwellTime, Map<String, String> customDataMap) {
-        sendCustomEvent(EVENT_PLACE_EXITED, zoneInfo, dwellTime, customDataMap);
-    }
-
-    @Override
-    public void onBlueDotPointServiceStartedSuccess() {
-        System.out.println("-- onBlueDotPointServiceStartedSuccess");
-        serviceManager.subscribeForApplicationNotification(this);
-    }
-
-    @Override
-    public void onBlueDotPointServiceStop() {
-        serviceManager.unsubscribeForApplicationNotification(this);
-    }
-
-    @Override
-    public void onBlueDotPointServiceError(BDError bdError) {
-
-    }
-
-    @Override
-    public void onRuleUpdate(List<ZoneInfo> list) {
-
-    }
-
-    /**
-     * Creates notification channel and notification, required for foreground service notification.
-     * @return notification
-     */
-    private Notification createNotification() {
-
-        String channelId, channelName;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channelId = "Bluedot" + getString(R.string.app_name);
-            channelName = "Bluedot Service" + getString(R.string.app_name);
-            notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.enableLights(false);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.enableVibration(false);
-            NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(notificationChannel);
-
-            Notification.Builder notification = new Notification.Builder(getApplicationContext(), channelId)
-                    .setContentTitle(getString(R.string.foreground_notification_title))
-                    .setContentText(getString(R.string.foreground_notification_text))
-                    .setStyle(new Notification.BigTextStyle().bigText(getString(R.string.foreground_notification_text)))
-                    .setOngoing(true)
-                    .setCategory(Notification.CATEGORY_SERVICE)
-                    .setSmallIcon(R.mipmap.ic_launcher);
-
-            return notification.build();
-        } else {
-
-            NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext())
-                    .setContentTitle(getString(R.string.foreground_notification_title))
-                    .setContentText(getString(R.string.foreground_notification_text))
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.foreground_notification_text)))
-                    .setOngoing(true)
-                    .setCategory(Notification.CATEGORY_SERVICE)
-                    .setPriority(PRIORITY_MAX)
-                    .setSmallIcon(R.mipmap.ic_launcher);
-
-            return notification.build();
-        }
     }
 
     private void requestPermissions() {
@@ -192,5 +83,16 @@ public class MainApplication extends Application implements ServiceStatusListene
         Intent intent = new Intent(getApplicationContext(), RequestPermissionActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onInitializationFinished(@Nullable BDError bdError) {
+        if (bdError != null){
+            Toast.makeText(getApplicationContext(),
+                    "Bluedot Initialization Error " + bdError.getReason(),
+                    Toast.LENGTH_LONG).show();
+
+            return;
+        }
     }
 }
